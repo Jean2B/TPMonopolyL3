86490614d65ebc70df6871433e1ee56bdd7ddda9
Jean Bertrand <90519788+Jean2B@users.noreply.github.com>
2022-03-29 11:07:57 +0200

    JSON

---
+++ it3/ParseJSON.java
@@ -0,0 +1,26 @@
+package it3;
+
+import java.io.InputStream;
+import org.json.*;
+
+public class ParseJSON {
+	private static final String FICHIER = "plateau.json";
+	
+	public static JSONObject getJSON() {
+        String resourceName = FICHIER;
+        InputStream is = ParseJSON.class.getResourceAsStream(resourceName);
+        if (is == null) {
+            throw new NullPointerException("Cannot find resource file " + resourceName);
+        }
+
+        JSONTokener tokener = new JSONTokener(is);
+        JSONObject object = new JSONObject(tokener);
+        return object;
+    }
+	
+	public static String getTypeCase(int nbCase) {
+		JSONObject json = getJSON();
+	    JSONObject c = json.getJSONObject("" + nbCase);
+	    return c.getString("type");
+	}
+}
